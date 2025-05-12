b0bff6a3e5b4b45e81ff20e7e0b79df9a7949aab
HaiNMN <hai23092003@gmail.com>
2025-05-12 23:15:09 +0700

    update

---
+++ featureLab/Assets/ClickGame_Jump.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ClickGame_Jump : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float rayLength = 0.2f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Kiểm tra nếu đang đứng trên mặt đất bằng Raycast
+        bool isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, rayLength, groundLayer);
+
+        // Vẽ tia ray trong Scene để dễ debug
+        Debug.DrawRay(groundCheck.position, Vector2.down * rayLength, Color.red);
+
+        // Chỉ nhảy nếu chạm đất
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
---
+++ featureLab/Assets/Scripts/CllickGame/Jump.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // kiem tra cham dat
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Nhấn phím Space để nhảy
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    // Vẽ vùng kiểm tra đất (gợi ý dùng trong Scene view)
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
